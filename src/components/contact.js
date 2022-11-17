00408db89085ed45eea8d9bbf1350c9611f564c1
MandyTrl <mandythorel@gmail.com>
2022-11-17 19:07:50 +0100

    modal contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,15 @@
+import TRex from '../ressources/trex.png';
+
 function Contact () {
    return (
-      <div className= "projects">
-         <h2>Contact</h2>
+      <div className= "contact">
+         <form method= "post">
+            <input type= "text" name= "name" placeholder= "Nom" required />
+            <input type= "email" name= "email" placeholder= "Email" required />
+            <textarea name= "message" placeholder= "Message" required />
+            <img className= "Trex" src= {TRex} alt= "trex" />
+            <button type= "submit" value="Contactez-moi">Contactez-moi</button>
+         </form>
       </div>
    );
   }
